774cc236a8e15a2ab209cc482b09b1bb72527ba6
nipajesmin <nipakterjesmin@gmail.com>
2025-01-24 01:21:57 +0600

    slidely change in style

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,23 @@ import { redirect } from "next/navigation";
 // export default function Home() {
 //   return (
 //     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+
 //     </div>
 //   );
 // }
 
-const Home = async () =>{
+const Home = async () => {
 
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
-  
 
-  
+
+
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Home Page</h1>
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-bold mt-4">Blog Page</h1>
+      </div>
       <div>
         <FeaturePosts data={data} />
       </div>
--- src/components/FeaturePosts.jsx
+++ src/components/FeaturePosts.jsx
@@ -2,20 +2,18 @@
 
 import { PostCard } from "./PostCard";
 
-const FeaturePosts = ({data}) => {
-    return(
-
-        <div className="grid grid-cols-3 gap-2">
+const FeaturePosts = ({ data }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 m-5">
             {data.map((post) => (
-                <PostCard key={post.id} post={post}/>
+                <PostCard key={post.id} post={post} />
             ))}
-
-
         </div>
     );
 };
 
 export default FeaturePosts;
+
 
 
 
